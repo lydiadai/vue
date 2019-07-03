c3cc3ea17145a14c74ed0c6a35a41517c0d498d0
liugang <501225658@qq.com>
2019-07-03 10:03:03 +0800

    say hi

---
+++ src/components/SayHi.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hi">
+    {{ welcome }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      welcome: 'Hi! I am LiuGang'
+    }
+  }
+}
+</script>
+
+<style>
+.hi {
+  color: red;
+  font-size: 20px;
+}
+</style>
